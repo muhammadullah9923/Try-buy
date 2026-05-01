f35f9be7427e6eb0a4a2802b1f090309105a9e50
muhammadullah9923 <muhammadullah9923@gmail.com>
2026-05-01 21:20:06 +0500

    cors allowes frontend netliify url added

--- SmartCar_backend/SmartCart/SmartCart/settings.py
+++ SmartCar_backend/SmartCart/SmartCart/settings.py
@@ -160,6 +160,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
+    "https://tryandbuyvton.netlify.app",
 ]
 CORS_ALLOW_HEADERS = [
     'accept',
